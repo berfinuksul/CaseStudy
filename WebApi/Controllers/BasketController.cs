c0e0ede7561d1a4e2cb4a2f16ada33740f73208d
Berfin UKŞUL <85320273+berfinuksul@users.noreply.github.com>
2023-01-15 03:44:08 +0300

    Add files via upload

---
+++ WebApi/Controllers/BasketController.cs
@@ -0,0 +1,67 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketsController : ControllerBase
+    {
+        private readonly IBasketService _basketService;
+
+        public BasketsController(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+        [HttpGet("all")]
+        public IActionResult GetAll()
+        {
+            var result = _basketService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
+        [HttpPost]
+        public IActionResult Add(Basket newBasket)
+        {
+            var result = _basketService.Create(newBasket);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPut]
+        public IActionResult Update(string id, Basket updatedBasket)
+        {
+            var result = _basketService.Update(id, updatedBasket);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(string id)
+        {
+            var result = _basketService.Delete(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
---
+++ WebApi/Controllers/ProductController.cs
@@ -0,0 +1,77 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("all")]
+        public IActionResult GetAll()
+        {
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product newProduct)
+        {
+            var result = _productService.Create(newProduct);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPut]
+        public IActionResult Update(string id, Product updatedProduct)
+        {
+            var result = _productService.Update(id, updatedProduct);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(string id)
+        {
+            var result = _productService.Delete(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("id")]
+        public IActionResult GetById(string id)
+        {
+            var result = _productService.Get(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
